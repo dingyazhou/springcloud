84134a41ea11bec6d201a90a6a4511b84acc010e
szmiao <szmiao0822>
2019-04-24 16:46:23 +0800

    Initial commit

---
+++ eureke/src/main/java/com/eureke/EurekeApplication.java
@@ -0,0 +1,13 @@
+package com.eureke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekeApplication.class, args);
+    }
+
+}
